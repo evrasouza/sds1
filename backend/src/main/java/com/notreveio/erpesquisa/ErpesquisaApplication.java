329eaf67629f64dca0ffa2108bfca83dd67e8abe
Everton Souza <evrasouza@gmail.com>
2021-12-05 11:12:21 -0300

    first commit

---
+++ backend/src/main/java/com/notreveio/erpesquisa/ErpesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.notreveio.erpesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/notreveio/erpesquisa/ErpesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notreveio.erpesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
